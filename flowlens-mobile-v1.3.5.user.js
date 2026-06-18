d193af5dca6e2b16e51850aa7e7118901b319838
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-18 11:26:30 +0800

    Add cache-busting mobile userscript v1.3.5

---
+++ flowlens-mobile-v1.3.5.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         瀑光 FlowLens 手机整合版
+// @namespace    local.flowlens.mobile.all
+// @version      1.3.5
+// @description  手机 Edge / Tampermonkey 整合版：主脚本、优化补丁、全局设置和知乎适配一键安装。
+// @match        *://*/*
+// @run-at       document-start
+// @noframes
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @connect      *
+// @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-v1.3.5.user.js
+// @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-v1.3.5.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-zhihu.user.js
+// ==/UserScript==
+
+/* FlowLens mobile v1.3.5 cache-busting installer. */
