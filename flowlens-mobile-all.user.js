da37d43a30b899034c3c1fc01b179c5e4342e971
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-18 15:29:13 +0800

    feat: sync mobile media controls to 1.4.7

--- flowlens-mobile-all.user.js
+++ flowlens-mobile-all.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         瀑光 FlowLens 手机整合版
 // @namespace    local.flowlens.mobile.all
-// @version      1.4.6
-// @description  手机 Edge / Tampermonkey 真整合版：同步电脑端 v1.4.6 修复，含去重、稳定大图、紧凑设置、1:1 居中。
+// @version      1.4.7
+// @description  手机 Edge / Tampermonkey 真整合版：同步电脑端 v1.4.7 功能，支持媒体切换按钮和大图视频自动播放。
 // @match        *://*/*
 // @run-at       document-idle
 // @noframes
@@ -13,16 +13,17 @@
 // @connect      *
 // @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-all.user.js
 // @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-all.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-fixes.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-product.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-ui-cleanup.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-center.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-fixes.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-product.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-ui-cleanup.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-center.user.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-media-sync.js?v=1.4.7
 // ==/UserScript==
 
-/* FlowLens mobile true integrated loader v1.4.6. Desktop and mobile entries are separated; shared fixes are loaded from the stable repository paths above. */
+/* FlowLens mobile true integrated loader v1.4.7. Desktop and mobile entries are separated; shared fixes are loaded from the stable repository paths above. */
