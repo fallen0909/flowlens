a4a4bdd7b918447b067d579e7bf156b9733ad6f9
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-18 11:31:01 +0800

    Add real mobile integrated userscript v1.3.7

---
+++ flowlens-mobile-v1.3.7.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         瀑光 FlowLens 手机整合版
+// @namespace    local.flowlens.mobile.all
+// @version      1.3.7
+// @description  手机 Edge / Tampermonkey 真整合版：同步电脑端最新修复，含去重、稳定大图、紧凑设置、1:1 居中。
+// @match        *://*/*
+// @run-at       document-idle
+// @noframes
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @connect      *
+// @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-v1.3.7.user.js
+// @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-v1.3.7.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-fixes.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-product.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-ui-cleanup.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-center.user.js
+// ==/UserScript==
+
+/*
+  FlowLens mobile true integrated loader v1.3.7.
+  重点：不要再用旧的 flowlens-mobile-all.user.js 缓存地址安装。
+  安装本文件前，请先在 Tampermonkey 删除旧版“瀑光 FlowLens 手机整合版”。
+*/
