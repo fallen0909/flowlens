fddcb1b784a9d9443a1d8d953eaa8b4245ab215f
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-18 15:28:41 +0800

    feat: sync desktop userscript media controls to 1.4.7

--- flowlens-desktop.user.js
+++ flowlens-desktop.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         瀑光 FlowLens 电脑油猴版
 // @namespace    local.flowlens.desktop
-// @version      1.4.6
-// @description  电脑 Edge / Chrome / Firefox + Tampermonkey 版：与电脑扩展版、手机整合版保持核心功能同步。
+// @version      1.4.7
+// @description  电脑 Edge / Chrome / Firefox + Tampermonkey 版：与手机整合版保持核心功能同步，支持媒体切换按钮和大图视频自动播放。
 // @match        *://*/*
 // @run-at       document-idle
 // @noframes
@@ -13,16 +13,17 @@
 // @connect      *
 // @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-desktop.user.js
 // @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-desktop.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-fixes.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-product.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-ui-cleanup.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-topfix.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-fixes.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-product.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-ui-cleanup.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-topfix.js?v=1.4.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-media-sync.js?v=1.4.7
 // ==/UserScript==
 
-/* FlowLens desktop userscript loader v1.4.6. Use this on desktop browsers when you prefer Tampermonkey auto-update over loading an unpacked extension. */
+/* FlowLens desktop userscript loader v1.4.7. Use this on desktop browsers when you prefer Tampermonkey auto-update over loading an unpacked extension. */
