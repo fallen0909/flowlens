d69dd5fbfb4f1be259939d39f126a23870b20f14
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-18 15:18:16 +0800

    feat: add desktop tampermonkey installer

---
+++ flowlens-desktop.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         瀑光 FlowLens 电脑油猴版
+// @namespace    local.flowlens.desktop
+// @version      1.4.6
+// @description  电脑 Edge / Chrome / Firefox + Tampermonkey 版：与电脑扩展版、手机整合版保持核心功能同步。
+// @match        *://*/*
+// @run-at       document-idle
+// @noframes
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @connect      *
+// @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-desktop.user.js
+// @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-desktop.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-fixes.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-product.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-ui-cleanup.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-topfix.js
+// ==/UserScript==
+
+/* FlowLens desktop userscript loader v1.4.6. Use this on desktop browsers when you prefer Tampermonkey auto-update over loading an unpacked extension. */
