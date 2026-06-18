197560d98126690c2f2050fcdb85e3e5f22c0527
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-18 11:32:16 +0800

    Add stable mobile latest userscript URL

---
+++ flowlens-mobile-latest.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         瀑光 FlowLens 手机整合版
+// @namespace    local.flowlens.mobile.all
+// @version      1.3.7
+// @description  手机 Edge / Tampermonkey 稳定更新版：以后固定使用本链接更新。
+// @match        *://*/*
+// @run-at       document-idle
+// @noframes
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @connect      *
+// @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-latest.user.js
+// @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-latest.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-fixes.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-product.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-ui-cleanup.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-center.user.js
+// ==/UserScript==
+
+/*
+  FlowLens mobile stable latest loader.
+  固定安装地址：
+  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-latest.user.js
+
+  以后手机版更新只维护这个文件，不再更换安装链接。
+*/
