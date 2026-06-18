27f881ad5ddb497c419dc63af66e0033ada03e97
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-18 12:33:49 +0800

    Update stable mobile loader to v1.4.1

--- flowlens-mobile-latest.user.js
+++ flowlens-mobile-latest.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         瀑光 FlowLens 手机整合版
 // @namespace    local.flowlens.mobile.all
-// @version      1.4.0
-// @description  手机 Edge / Tampermonkey 稳定更新版：顶部信息纯文字、图片贴顶、大图不闪、自动切换按钮强制显示、增强媒体抓取。
+// @version      1.4.1
+// @description  手机 Edge / Tampermonkey 稳定更新版：修复左上角字号、自动切换按钮、1:1 手动移动、增强媒体抓取。
 // @match        *://*/*
 // @run-at       document-idle
 // @noframes
@@ -22,10 +22,8 @@
 // @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js
 // @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js
 // @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-center.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-polish.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-layout-v2.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-media-bridge.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-media-bridge-v2.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-final-fixes.user.js
 // ==/UserScript==
 
 /*
