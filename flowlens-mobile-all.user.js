3c3a02055e6e2f1c4a63b6578b897ba8936e2102
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-18 11:31:14 +0800

    Point mobile all loader to true integrated v1.3.7

--- flowlens-mobile-all.user.js
+++ flowlens-mobile-all.user.js
@@ -1,23 +1,28 @@
 // ==UserScript==
 // @name         瀑光 FlowLens 手机整合版
 // @namespace    local.flowlens.mobile.all
-// @version      1.3.6
-// @description  手机 Edge / Tampermonkey 整合版：1:1 原尺寸放大后自动居中。
+// @version      1.3.7
+// @description  手机 Edge / Tampermonkey 真整合版：同步电脑端最新修复，含去重、稳定大图、紧凑设置、1:1 居中。
 // @match        *://*/*
-// @run-at       document-start
+// @run-at       document-idle
 // @noframes
 // @grant        GM_xmlhttpRequest
 // @grant        GM_download
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @connect      *
-// @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-v1.3.6.user.js
-// @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-v1.3.6.user.js
+// @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-v1.3.7.user.js
+// @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-v1.3.7.user.js
 // @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-global-settings.user.js
 // @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens.user.js
 // @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-optimizer.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-zhihu.user.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-fixes.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-product.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-ui-cleanup.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-lightbox-stable.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-settings-compact-v2.js
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-extension/content-zhihu.js
 // @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/mobile-userscript/flowlens-mobile-center.user.js
 // ==/UserScript==
 
-/* FlowLens mobile all-in-one loader v1.3.6. */
+/* FlowLens mobile true integrated loader v1.3.7. */
