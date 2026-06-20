8f0ec023f716141a847ecf570f2d098a7ac118af
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-20 11:10:25 +0800

    Add extension version center content script

---
+++ flowlens-extension/content-version.js
@@ -0,0 +1,28 @@
+(() => {
+  const VERSION = "1.4.44";
+  const CHANNEL = "stable";
+  const RELEASE_DATE = "2026-06-20";
+  const FEATURES = [
+    "unified-version-center",
+    "video-cover-strategy"
+  ];
+
+  const previous = window.__FlowLensVersion && typeof window.__FlowLensVersion === "object"
+    ? window.__FlowLensVersion
+    : {};
+
+  const info = Object.freeze({
+    ...previous,
+    name: "瀑光 FlowLens",
+    version: VERSION,
+    channel: CHANNEL,
+    releaseDate: RELEASE_DATE,
+    features: Object.freeze([...(Array.isArray(previous.features) ? previous.features : []), ...FEATURES]
+      .filter((item, index, array) => item && array.indexOf(item) === index)),
+    source: "flowlens-extension/content-version.js"
+  });
+
+  window.__FlowLensVersion = info;
+  window.__FLOWLENS_VERSION__ = VERSION;
+  window.__flowLensGetVersion = () => window.__FlowLensVersion || info;
+})();
