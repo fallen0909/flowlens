c12558b1f75cdaad3510d314d018bebf876e9dda
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-24 21:10:03 +0800

    Bump mobile entry to v1.7.7

--- flowlens-mobile-all.user.js
+++ flowlens-mobile-all.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         瀑光 FlowLens 手机整合版
 // @namespace    local.flowlens.mobile.all
-// @version      1.7.6
-// @description  正式安装版：沉浸式网页图片与视频瀑布流，增强大图自动播放和快捷放大。
+// @version      1.7.7
+// @description  正式安装版：沉浸式网页图片与视频瀑布流，修复 x810114 刷新自启和大图按钮同步。
 // @match        *://*/*
 // @run-at       document-idle
 // @noframes
@@ -13,22 +13,23 @@
 // @connect      *
 // @downloadURL  https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-all.user.js
 // @updateURL    https://raw.githubusercontent.com/fallen0909/flowlens/master/flowlens-mobile-all.user.js
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/core/version.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/core/global-settings.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/item-gallery.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/core/flowlens-core.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/core/optimizer.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/product.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/fixes.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/ui-cleanup.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/lightbox-stable.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/settings-compact.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/zhihu.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/topfix.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/media-sync.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/lightbox-enhance.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/page-bookmarks.js?v=1.7.6
-// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/mobile/mobile-center.js?v=1.7.6
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/core/version.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/core/global-settings.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/x810114-safe-start.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/item-gallery.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/core/flowlens-core.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/core/optimizer.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/product.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/fixes.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/ui-cleanup.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/lightbox-stable.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/settings-compact.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/zhihu.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/topfix.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/media-sync.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/lightbox-enhance.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/patches/page-bookmarks.js?v=1.7.7
+// @require      https://raw.githubusercontent.com/fallen0909/flowlens/master/src/mobile/mobile-center.js?v=1.7.7
 // ==/UserScript==
 
 (() => {
@@ -36,10 +37,10 @@
   window.__FlowLensVersion = Object.freeze({
     ...previous,
     name: "瀑光 FlowLens",
-    version: "1.7.6",
+    version: "1.7.7",
     channel: "stable",
     releaseDate: "2026-06-24",
     entry: "flowlens-mobile-all.user.js"
   });
-  window.__FLOWLENS_VERSION__ = "1.7.6";
+  window.__FLOWLENS_VERSION__ = "1.7.7";
 })();
