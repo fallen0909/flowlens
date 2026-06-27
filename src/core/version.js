64e652d5a83f7cf23f8291ba70c82230a4d343a1
fallen0909 <135415010+fallen0909@users.noreply.github.com>
2026-06-27 21:17:10 +0800

    Set version center 1.7.25

--- src/core/version.js
+++ src/core/version.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FlowLens version center
 // @namespace    local.flowlens.version
-// @version      1.7.24
+// @version      1.7.25
 // @description  FlowLens runtime version center.
 // @match        *://*/*
 // @run-at       document-start
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 (() => {
-  const VERSION = "1.7.24";
+  const VERSION = "1.7.25";
   const CHANNEL = "stable";
   const RELEASE_DATE = "2026-06-26";
   const FEATURES = [
@@ -30,8 +30,7 @@
     "lightbox-stable-toolbar",
     "xchina-ad-filter",
     "media-filter-center",
-    "virtual-masonry",
-    "filtered-sequence"
+    "virtual-masonry"
   ];
 
   const previous = window.__FlowLensVersion && typeof window.__FlowLensVersion === "object" ? window.__FlowLensVersion : {};
